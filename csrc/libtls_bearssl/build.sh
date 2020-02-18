33c9783217502e286cea30bd94e682ff71238a2f
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2020-02-18 12:07:40 +0200

    unimportant

--- csrc/libtls_bearssl/build.sh
+++ csrc/libtls_bearssl/build.sh
@@ -1,4 +1,5 @@
-${X}gcc -c *.c compat/*.c -Wall -Wpedantic -D_POSIX_SOURCE -I. -I../bearssl -O2 -fPIC $C
+${X}gcc -c *.c compat/*.c -I. -I../bearssl -O2 -fPIC $C \
+	-Wall -Wpedantic -D_FORTIFY_SOURCE=1 -D_POSIX_SOURCE
 ${X}gcc *.o -shared -o ../../bin/$P/$D -L../../bin/$P -lbearssl $L
 rm -f      ../../bin/$P/$A
 ${X}ar rcs ../../bin/$P/$A *.o
